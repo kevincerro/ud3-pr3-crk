86759c43565f14b5653f555ede7fa66816e08da2
Kevin <kevincerro1997@gmail.com>
2022-05-05 21:41:26 +0200

    Show location, age and emotion

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 1. Create virtual env: `python3 -m venv venv`
 2. Activate created virtual env: `. venv/bin/activate`
 3. Install dependencies: `pip install -r requirements.txt`
-5. Run this command in console: `flask run`
+4. Run this command in console: `flask run`
+
+# Sample images to test
+- https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg
+- https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/Face/images/test-image-person-group.jpg
--- services/face_service.py
+++ services/face_service.py
@@ -4,13 +4,45 @@ from msrest.authentication import CognitiveServicesCredentials
 
 
 def get_face_client():
-    return FaceClient(os.environ.get('AZURE_FACE_ENDPOINT'), CognitiveServicesCredentials(os.environ.get('AZURE_FACE_KEY')))
+    return FaceClient(
+        os.environ.get('AZURE_FACE_ENDPOINT'),
+        CognitiveServicesCredentials(os.environ.get('AZURE_FACE_KEY'))
+    )
+
 
 def detect_faces_in_image(url: str):
     face_client = get_face_client()
-    detected_faces = face_client.face.detect_with_url(url=url, detection_model='detection_03')
+
+    face_attributes = ['age', 'emotion']
+    detected_faces = face_client.face.detect_with_url(url=url, return_face_attributes=face_attributes)
 
     if not detected_faces:
         raise Exception('No face detected.')
 
+    # Find emotion with better score
+    for face in detected_faces:
+        emotions = face.face_attributes.emotion
+        face.emotion = get_most_scored_emotion(emotions)
+
     return detected_faces
+
+
+def get_most_scored_emotion(emotions):
+    scores = {
+        'anger': emotions.anger,
+        'contempt': emotions.contempt,
+        'disgust': emotions.disgust,
+        'fear': emotions.fear,
+        'happiness': emotions.happiness,
+        'neutral': emotions.neutral,
+        'sadness': emotions.sadness,
+        'surprise': emotions.surprise,
+    }
+
+    name = max(scores, key=scores.get)
+    score = scores[name]
+
+    return {
+        'name': name,
+        'score': score,
+    }
